4b64221eb102fbe4ab3397eb83a0dd011462b94e
Prosper <prosper@benoble.io>
2024-08-27 17:51:22 -0300

    modified helper functions and added base64parser

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,23 @@
-export function capitalizeFirstLetter(string: string) {
+export function capitalizeFirstLetter(string: string): string {
   return string?.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function lowercaseFirstLetter(string: string) {
+export function lowercaseFirstLetter(string: string): string {
   return string?.charAt(0).toLowerCase() + string.slice(1);
 }
+
+export const processBase64Image = (
+  base64: string
+): { type: string; data: string } | null => {
+  const base64Regex = /^data:image\/(png|jpeg|jpg|gif|bmp|webp);base64,/;
+  const match = base64.match(base64Regex);
+
+  if (!match) {
+    return null;
+  }
+
+  const mimeType = match[1];
+  const data = base64.split(",")[1];
+
+  return { type: mimeType, data };
+};
