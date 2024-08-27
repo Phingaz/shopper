ecf81ccd3f70b3530b97d87837420c5839244a0d
Prosper <prosper@benoble.io>
2024-08-27 17:50:27 -0300

    added minio for cloud storage and public link generation

---
+++ src/utils/minio.ts
@@ -0,0 +1,27 @@
+import * as Minio from "minio";
+import envValues from "./env";
+
+const minioClient = new Minio.Client({
+  useSSL: false,
+  endPoint: envValues.minioApiEndpoint,
+  port: envValues.minioApiPort,
+  accessKey: envValues.minioAccessKey,
+  secretKey: envValues.minioSecretKey,
+});
+
+export const bucketName = "shopper";
+
+export const getBucket = async () => {
+  try {
+    const exists = await minioClient.bucketExists(bucketName);
+    if (!exists) {
+      await minioClient.makeBucket(bucketName);
+    } else {
+      console.log("Bucket already exists.");
+    }
+  } catch (error) {
+    throw new Error(`Failed to create bucket: ${error.message}`);
+  }
+};
+
+export default minioClient;
