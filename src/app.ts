0645ff5f9534b07262178df612da9f0530c6b8e0
Prosper <prosper@benoble.io>
2024-08-27 13:55:43 -0300

    increased json body limit

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ import { errorHandler, successHandler } from "./utils/resHandler.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false, limit: "1mb" }));
+app.use(express.urlencoded({ extended: false, limit: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
 app.use(express.static("public"));
 app.use(expectJsonBody);
 
