dbb07a791c1c4351453d9ab79d2bf3044625602f
Prosper <prosper@benoble.io>
2024-08-27 17:51:44 -0300

    added vars for other env variables

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,9 @@
 const envValues = {
   geminiKey: process.env.GEMINI_API_KEY,
+  minioAccessKey: process.env.MINIO_ROOT_USER,
+  minioSecretKey: process.env.MINIO_ROOT_PASSWORD,
+  minioApiEndpoint: process.env.MINIO_ENDPOINT,
+  minioApiPort: parseInt(process.env.MINIO_PORT),
 };
 
 export default envValues;
